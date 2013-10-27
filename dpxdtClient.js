b8c5a0de1942c56d5f346281bc5dfda51289fd7c
Riley <rileyel@haikulearning.com>
2013-10-27 13:58:55 -0500

    In which Riley learns how to use commander

--- dpxdtClient.js
+++ dpxdtClient.js
@@ -3,10 +3,9 @@ var program = require('commander');
 
 program
     .version('0.0.0')
-    .option('-t, --test-file <path>', 'JSON file describing all of your tests')
-    .option('-d, --dpxdt-url <url>', 'Base url for the dpxdt server. Defaults to http://localhost:5000')
-    .option('-r, --release-name <name>', 'Name of the release for which you want to queue all of your tests')
-    .option('-b, --build-number <number>', 'Build number for the dpxdt server.  Defaults to 1.');
+    .option('-d, --dpxdt-url <url>', 'Base url for the dpxdt server. Defaults to http://localhost:5000', 'http://localhost:5000')
+    .option('-r, --release-name <name>', 'Name of the release for which you want to queue all of your tests', (new Date()).toJSON())
+    .option('-b, --build-number <number>', 'Build number for the dpxdt server.  Defaults to 1.', 1);
 
 program
     .command('new <testFileName>')
@@ -27,15 +26,6 @@ program.parse(process.argv);
 
 
 function getCommands() {
-
-    var dpxdtUrl = !!program.dpxdtUrl ? program.dpxdtUrl : 'http://localhost:5000';
-    var releaseName = !!program.releaseName ? program.releaseName : (new Date()).toJSON();
-    var buildNumber = !!program.buildNumber ? program.buildNumber : 1;
-
-    if (!dpxdtUrl || !releaseName || !buildNumber) {
-        program.help(); //exits immediately
-    }
-
-    var commands = require('./commands')(dpxdtUrl, buildNumber, releaseName);
+    var commands = require('./commands')(program.dpxdtUrl, program.buildNumber, program.releaseName);
     return commands;
 }
